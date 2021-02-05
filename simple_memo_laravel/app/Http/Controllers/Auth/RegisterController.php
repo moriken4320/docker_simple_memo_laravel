3ecf175e4f6a659125411a316e5fc43455db03b2
moriken4320 <moriken.4320@gmail.com>
2021-02-05 12:17:12 +0900

    エラー文を日本語化

--- simple_memo_laravel/app/Http/Controllers/Auth/RegisterController.php
+++ simple_memo_laravel/app/Http/Controllers/Auth/RegisterController.php
@@ -54,6 +54,10 @@ class RegisterController extends Controller
             'name' => 'required|max:255|regex:/^[a-zA-Z0-9]+$/',
             'email' => 'required|max:255|email|unique:users',
             'password' => 'required|max:255|min:8|regex:/^[a-zA-Z0-9]+$/',
+        ],
+        [
+            'name.regex' => ':attributeは半角英数字で入力してください。',
+            'password.regex' => ':attributeは半角英数字で入力してください。',
         ]);
 
         User::create([
