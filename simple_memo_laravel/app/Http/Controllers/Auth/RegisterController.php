0b116245e3b9323045be7ac35f28e87707e0c3ae
moriken4320 <moriken.4320@gmail.com>
2021-02-05 12:08:12 +0900

    コントローラーに直接バリデーションを記述する方法

--- simple_memo_laravel/app/Http/Controllers/Auth/RegisterController.php
+++ simple_memo_laravel/app/Http/Controllers/Auth/RegisterController.php
@@ -50,6 +50,12 @@ class RegisterController extends Controller
      */
     public function register(Request $request)
     {
+        $request->validate([
+            'name' => 'required|max:255|regex:/^[a-zA-Z0-9]+$/',
+            'email' => 'required|max:255|email|unique:users',
+            'password' => 'required|max:255|min:8|regex:/^[a-zA-Z0-9]+$/',
+        ]);
+
         User::create([
             'name' =>  $request->name,
             'email' => $request->email,
