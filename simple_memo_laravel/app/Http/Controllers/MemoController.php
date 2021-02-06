4e4bac71c37464f056ed7a89988a3306c954cc68
moriken4320 <moriken.4320@gmail.com>
2021-02-06 12:58:45 +0900

    メモ選択機能を実装

--- simple_memo_laravel/app/Http/Controllers/MemoController.php
+++ simple_memo_laravel/app/Http/Controllers/MemoController.php
@@ -18,7 +18,8 @@ class MemoController extends Controller
 
         return view('memo', [
             'name' => $this->getLoginUserName(),
-            'memos' => $memos
+            'memos' => $memos,
+            'select_memo' => session()->get('select_memo')
         ]);
     }
 
@@ -36,6 +37,19 @@ class MemoController extends Controller
 
         return redirect()->route('memo.index');
     }
+
+    /**
+     * メモの選択
+     * @param Request $request
+     * @return \Illuminate\Http\RedirectResponse
+     */
+     public function select(Request $request)
+     {
+         $memo = Memo::find($request->id);
+         session()->put('select_memo', $memo);
+
+         return redirect()->route('memo.index');
+     }
 
     /**
      * ログインユーザー名取得
